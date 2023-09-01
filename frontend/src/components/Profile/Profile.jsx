c39d447231ba8381aa54c63c94c859b084bae4e0
Andrew Senichkin <Kniws10@yandex.ru>
2023-09-02 00:02:46 +0300

    fix validation

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,85 +1,109 @@
-import {CurrentUserContext} from "../../context/CurrentUserContext";
-import React, {useContext, useEffect, useState} from "react";
-import Forms from "../../utils/validation";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+import React, { useContext, useEffect, useState } from "react";
+import useForm from "../../utils/validation";
 import "./Profile.css";
 import { EMAIL_PATTERN, USERNAME_PATTERN } from "../../utils/constants";
 
-function Profile(props) {
-    const {values, errors, handleChange, handleSubmit} = Forms(props.updateUser);
-    const [currentUserEdit, setcurrentUserEdit] = useState(false);
-    const currentUser = useContext(CurrentUserContext);
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
+function Profile({ isLoading, signOut, onUpdateUser}) {
+  const currentUser = useContext(CurrentUserContext);
 
-    useEffect(() => {
-        setName(currentUser.name);
-        setEmail(currentUser.email);
-    }, [currentUser]);
+  const { enteredValues, handleChangeInput, isFormValid, resetForm } =
+    useForm();
 
-    useEffect(() => {
-        if ((values.name !== undefined || values.email !== undefined)) setcurrentUserEdit(true);
-        else setcurrentUserEdit(false);
-    }, [currentUser, name, email, values.name, values.email]);
+  // Состояние для отслеживания изменений в значениях полей формы
+  const [isLastValues, setIsLastValues] = useState(false);
 
-    return (
-        <main>
-            <section className="profile">
-                <h1 className="profile__title">Привет, {currentUser.name}!</h1>
-                <form
-                    name="user-data"
-                    className="profile__form form"
-                    onSubmit={handleSubmit}>
-                    <label className="profile__label" htmlFor="name">
-                        <span className="profile__subtitle">Имя</span>
-                        <input
-                            defaultValue={values.name || name}
-                            className="profile__input"
-                            onChange={handleChange}
-                            placeholder="Имя"
-                            maxLength="30"
-                            minLength="2"
-                            type="text"
-                            name="name"
-                            required
-                            pattern={USERNAME_PATTERN}
-                        />
-                    </label>
-                    <label className="profile__label" htmlFor="email">
-                        <span className="profile__subtitle profile__subtitle_bottom">E-mail</span>
-                        <input
-                            className="profile__input profile__input_bottom"
-                            defaultValue={values.email || email}
-                            onChange={handleChange}
-                            placeholder="E-mail"
-                            type="email"
-                            name="email"
-                            required
-                            pattern={EMAIL_PATTERN}
-                        />
-                    </label>
-                    <button
-                        className={`profile__btn profile__btn_type_edit  ${
-                            (errors.email
-                                || errors.name
-                                || values.email === email
-                                || values.name === name
-                                || !currentUserEdit)
-                            && "profile__btn_disable"
-                        }`}
-                        type="submit">
-                        Редактировать
-                    </button>
-                </form>
-                <button
-                    className="profile__btn profile__btn_type_logout"
-                    onClick={props.onLogin}
-                    type="button"
-                >
-                    Выйти из аккаунта
-                </button>
-            </section>
-        </main>
-    );
+  // Сброс формы при обновлении текущего пользователя
+  useEffect(() => {
+    if (currentUser) {
+      resetForm(currentUser);
+    }
+  }, [currentUser, resetForm]);
+
+  // Обработка отправки формы
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    onUpdateUser({
+      name: enteredValues.name,
+      email: enteredValues.email,
+    });
+  }
+
+  // Проверка, являются ли текущие значения полей формы последними сохраненными значениями
+  useEffect(() => {
+    if (
+      currentUser.name === enteredValues.name &&
+      currentUser.email === enteredValues.email
+    ) {
+      setIsLastValues(true);
+    } else {
+      setIsLastValues(false);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [enteredValues]);
+
+  return (
+    <main>
+      <section className="profile">
+        <h1 className="profile__title">Привет, {currentUser.name}!</h1>
+        <form
+          name="user-data"
+          className="profile__form form"
+          onSubmit={handleFormSubmit}
+          noValidate
+        >
+          <label className="profile__label" htmlFor="name">
+            <span className="profile__subtitle">Имя</span>
+            <input
+              className="profile__input"
+              placeholder="Имя"
+              maxLength="30"
+              minLength="2"
+              type="text"
+              name="name"
+              required
+              value={enteredValues.name || ""}
+              pattern={USERNAME_PATTERN}
+              onChange={handleChangeInput}
+            />
+          </label>
+          <label className="profile__label" htmlFor="email">
+            <span className="profile__subtitle profile__subtitle_bottom">
+              E-mail
+            </span>
+            <input
+              className="profile__input profile__input_bottom"        
+              onChange={handleChangeInput}
+              placeholder="E-mail"
+              type="email"
+              name="email"
+              required
+              value={enteredValues.email || ""}
+              pattern={EMAIL_PATTERN}
+            />
+          </label>
+          <button
+            disabled={!isFormValid ? true : false}
+            className={isFormValid || isLoading || isLastValues
+                        ? "profile__btn profile__btn_type_edit"
+                        : "profile__btn_disable"
+                      }
+            type="submit"
+          >
+            Редактировать
+          </button>
+        </form>
+        <button
+          className="profile__btn profile__btn_type_logout"
+          onClick={signOut}
+          type="button"
+        >
+          Выйти из аккаунта
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default Profile;
--- frontend/src/utils/validation.js
+++ frontend/src/utils/validation.js
@@ -1,51 +1,57 @@
-import {useState, useEffect} from "react";
+import { useState, useCallback } from "react";
 
-const Forms = (onLogin) => {
-    const [inactive, setInactive] = useState(null);
-    const [values, setValues] = useState({});
-    const [errors, setErrors] = useState({});
-
-    useEffect(() => {
-        setValues({});
-        setErrors({});
-        setInactive(false);
-    }, []);
-
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setValues({
-            ...values,
-            [name]: value,
-        });
-
-        setErrors({
-            ...errors,
-            [name]: event.target.validationMessage,
-        });
-
-        setInactive(event.target.closest(".form").checkValidity());
-    };
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        onLogin(values);
+//Hook useForm для управления состоянием формы
+const useForm = () => {
+    //Состояние для хранения введенных значений полей формы.
+    const [enteredValues, setEnteredInputValues] = useState({})
+    //Состояние для хранения ошибок валидации полей формы.
+    const [errors, setErrors] = useState({})
+    //Состояние, указывающее на валидность всей формы.
+    const [isFormValid, setIsFormValid] = useState(false)
+  
+    //Обработчик изменений полей формы. Обновляет состояния
+    // введенных значений, ошибок валидации и валидности формы.
+    const handleChangeInput = (event) => {
+      const name = event.target.name
+      const value = event.target.value
+  
+      // Обновляем состояние введенных значений
+      setEnteredInputValues({
+        ...enteredValues,
+        [name]: value,
+      })
+  
+      // Обновляем состояние ошибок валидации
+      setErrors({
+        ...errors,
+        [name]: event.target.validationMessage,
+      })
+  
+      // Обновляем состояние валидности формы
+      setIsFormValid(event.target.closest(".form").checkValidity())
     }
-
-    const resetForm = () => {
-        setValues({});
-        setErrors({});
-        setInactive(false);
-    }
-
+  
+    //Функция для сброса формы. Устанавливает новые значения состояний формы, переданные в аргументах.
+    const resetForm = useCallback(
+      (newValues = {}, newErrors = {}, newIsFormValid = false) => {
+        // Устанавливаем новые значения состояний формы
+        setEnteredInputValues(newValues)
+        setErrors(newErrors)
+        setIsFormValid(newIsFormValid)
+      },
+      [setEnteredInputValues, setErrors, setIsFormValid]
+    )
+    // Возвращаем состояния и функции для управления формой
     return {
-        values,
-        errors,
-        inactive,
-        handleChange,
-        handleSubmit,
-        resetForm,
-        setValues
-    };
-}
-
-export default Forms;
+      enteredValues,
+      handleChangeInput,
+      isFormValid,
+      errors,
+      resetForm,
+    }
+  }
+  
+  //Код представляет собой хук (hook) useForm, который упрощает
+  // управление состоянием и валидацией формы в React-компонентах.
+  
+  export default useForm;
