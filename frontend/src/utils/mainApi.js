62b5563b20eb2456a162e6c54e542d1e6285baf8
Andrew Senichkin <Kniws10@yandex.ru>
2023-09-13 14:02:01 +0300

    fix api

--- frontend/src/utils/mainApi.js
+++ frontend/src/utils/mainApi.js
@@ -1,88 +1,112 @@
-import {moviesApi} from "./constants";
-import {baseUrl} from "./constants";
+import { checkResponse } from './utils';
 
-const checkResponse = (res) => {
-    if (res.ok) return res.json();
-    else return Promise.reject(`${res.status} ${res.statusText}`);
-}
+export const BASE_URL = 'http://localhost:3000';
 
-export async function getUserInfo() {
-    const data = await fetch(`${baseUrl}/users/me`, {
-        method: "GET",
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('jwt')}`,
-            "Content-Type": "application/json",
-        },
-    })
-    return checkResponse(data);
-}
+//export const BASE_URL = 'https://api.kniwsdiplom.nomoredomains.xyz';
 
-export async function updateUserInfo(userInfo) {
-    const data = await fetch(`${baseUrl}/users/me`, {
-        method: "PATCH",
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('jwt')}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userInfo),
-    })
-    return checkResponse(data);
-}
+export const register = (name, email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name, email, password }),
+  }).then((res) => checkResponse(res));
+};
 
-export async function getMovies() {
-    const data = await fetch(`${baseUrl}/movies`, {
-        method: "GET",
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('jwt')}`,
-            "Content-Type": "application/json",
-        },
-    });
-    return checkResponse(data);
-}
+export const authorize = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => checkResponse(res));
+};
 
-export async function saveMovie({
-                                    country,
-                                    director,
-                                    duration,
-                                    year,
-                                    description,
-                                    image,
-                                    nameEN,
-                                    nameRU,
-                                    trailerLink,/*trailerLink*/ 
-                                    id
-                                }) {
+export const getContent = (token) => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => checkResponse(res));
+};
 
-    const data = await fetch(`${baseUrl}/movies`, {
-        method: "POST",
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('jwt')}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            country: country,
-            director: director,
-            duration: duration,
-            year: year,
-            description: description,
-            image: `${moviesApi}${image.url}`,
-            nameEN: nameEN,
-            nameRU: nameRU,
-            trailer: trailerLink,/*trailerLink*/
-            thumbnail: `${moviesApi}${image.formats.thumbnail.url}`,
-            movieId: id,
-        }),
-    });
-    return checkResponse(data);
-}
+// метод делает запрос серверу и получает данные профиля
+export const getUserInfo = () => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => checkResponse(res));
+};
 
-export async function deleteMovies(movieId) {
-    const data = await fetch(`${baseUrl}/movies/${movieId} `, {
-        method: "DELETE",
-        headers: {
-            authorization: `Bearer ${localStorage.getItem('jwt')}`,
-            "Content-Type": "application/json",
-        },
-    })
-    return checkResponse(data);
-}
+// метод изменяет данные профиля на сервере
+export const setUserInfo = (data) => {
+  // console.log(data);
+  return fetch(`${BASE_URL}/users/me`, {
+    method: 'PATCH', //метод запроса
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    }, //заголовки запроса
+    body: JSON.stringify({
+      //тело запроса
+      name: data.name, //в name передаем значение name объекта, переданного в setUserInfo
+      email: data.email, //в about передаем значение about объекта, переданного в setUserInfo
+    }),
+  }).then((res) => checkResponse(res));
+};
+
+export const getMovies = () => {
+  return fetch(`${BASE_URL}/movies`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => checkResponse(res));
+};
+
+// метод добавления новой карточки на сервер
+export const postCard = (data) => {
+  // console.log(data);
+  return fetch(`${BASE_URL}/movies`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      country: data.country,
+      director: data.director,
+      duration: data.duration,
+      year: data.year,
+      description: data.description,
+      image: 'https://api.nomoreparties.co' + data.image.url,
+      trailerLink: data.trailerLink,
+      thumbnail: 'https://api.nomoreparties.co' + data.image.formats.thumbnail.url,
+      movieId: data.id,
+      nameRU: data.nameRU,
+      nameEN: data.nameEN,
+    }),
+  }).then((res) => checkResponse(res));
+};
+
+// метод удаления карточки с сервера
+export const deleteCard = (cardId) => {
+  return fetch(`${BASE_URL}/movies/${cardId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => checkResponse(res));
+};
